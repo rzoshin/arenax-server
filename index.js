0fe8e217509b2372d5855a97344836eead7b2b68
rzoshin <raiyan435jgcc@gmail.com>
2026-05-18 19:20:11 +0600

    Set connection with mongodb and then installed better auth

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
+// Express server setup with CORS and MongoDB connection
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// Dotenv setup
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Load environment variables from .env file
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT || 8000;
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+
+// MongoDB client setup
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db("arenax");
+    const facilitiesCollection = db.collection("facilities");
+    const bookingCollection = db.collection("bookings");
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Arena X Server is running!')
@@ -9,3 +50,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
